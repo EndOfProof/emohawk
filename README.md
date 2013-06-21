51b648b1e0db51bc6f77f5da09628d6275d53407
Mike Brown <m.brown@epigenesys.co.uk>
2013-06-21 18:32:06 +0100

    Initial setup of the rails-api

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-emohawk
+Emohawk
 =======
+
+A game about creatures. Lots of creatures. And blood; there's blood too.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_toke to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Emohawk::Application.config.secret_token = 'fa4c6527b1dbf9ecf5d161928745d15c05bc1371494085d034c6abc32feac47088bc71695af2d549636449ba3da1d42afcc7d386a7add713db6205dbf7650be4'
