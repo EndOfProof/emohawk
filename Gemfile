b1f0d835b425fa31d4ca977c66ca2548e81203aa
Mike Brown <m.brown@epigenesys.co.uk>
2013-06-29 12:26:59 +0100

    Add state machine code to the creature

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'sqlite3'
 
 gem 'simple_enum'
 
+gem 'state_machine'
+
 group :development, :test do
   gem 'rspec'
   gem 'rspec-rails'
@@ -27,18 +29,3 @@ group :test do
 
   gem 'simplecov'
 end
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano', :group => :development
-
-# To use debugger
-# gem 'ruby-debug19', :require => 'ruby-debug'
--- app/models/creature.rb
+++ app/models/creature.rb
@@ -16,4 +16,48 @@ class Creature < ActiveRecord::Base
 		return children_of_mother if gender == :female
 		return children_of_father if gender == :male
 	end
+
+  state_machine :state, :initial => :ready do
+    event :begin_training do
+      transition ready: :training
+    end
+
+    event :finish_training do
+      transition training: :ready
+    end
+
+    event :begin_breeding do
+      transition ready: :breeding
+    end
+
+    event :finish_breeding do
+      transition breeding: :ready
+    end
+
+    event :become_pregnant do
+      transition breeding: :pregnant
+    end
+
+    event :birth do
+      transition pregnant: :ready
+    end
+
+    state :ready do
+
+    end
+
+    state :training do
+
+    end
+
+    state :breeding do
+
+    end
+
+    state :pregnant do
+      validate do |creature|
+        errors.add(:base, 'Only female creatures can become pregnant') unless creature.female?
+      end
+    end
+  end
 end
